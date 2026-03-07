00d77e5af6102d36735e8cf54df1fdd795478b7f
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-03-07 06:46:47 -0500

    added routes and vars

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen b">
+
+    {/* Theme toggle */ }
+    
+    {/* Background Effects*/}
+
+    {/* Navbar */}
+
+    { /* Main Content */}
+
+    {/* Footer */ }
+  </div>
+  );
+};
