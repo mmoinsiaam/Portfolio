68f4888203f768017e437d8f4f4c47d4284e4e8c
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-03-14 17:32:09 -0400

    added ThemeToggle component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,38 @@
-export const ThemeToggle = () => {
-    
+import { Sun, Moon } from "lucide-react";
+import { useEffect, useState } from "react";
 
-    return <button></button>
-}
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme === "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.add("dark");
+        } else {
+            setIsDarkMode(false);
+            document.documentElement.classList.remove("dark");  
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+        }
+    };
+
+
+    return <button onClick={toggleTheme}> 
+        {isDarkMode ? (
+            <Sun className="h-6 w-6 text-yellow-300" />
+        ) : (
+             <Moon className="h-6 w-6 text-blue-900" />
+        )}
+        </button>
+};
