412da9b3ea8b64098865b368a7c671767767590d
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-03-19 00:39:39 -0400

    completed and pushed AboutMe Section

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,7 @@
+
+
+export const SkillsSection = () => {
+    return (
+        <div></div>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection.jsx";
 
 export const Home = () => {
   return (
@@ -16,6 +18,8 @@ export const Home = () => {
     <main>
       {/* Hero Section */}
       <HeroSection />
+      <AboutSection />
+      <SkillsSection />
     </main>
     {/* Footer */ }
   </div>
