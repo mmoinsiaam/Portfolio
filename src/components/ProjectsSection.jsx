6e3cfa6c80fadd08e4c8820b2a988065316d1dd0
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-03-21 13:26:31 -0400

    clicking projects takes you to link now

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -52,7 +52,7 @@ export const ProjectsSection = () => {
                             className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
                         >
                             <div className="h-48 overflow-hidden">
-                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                                <img src={project.image} href={project.githubUrl}  alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
                             </div>
 
                             <div className="p-6">
