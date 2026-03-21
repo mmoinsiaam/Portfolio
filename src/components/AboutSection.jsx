b67899d8864ccc1e150f9cb9645b432f9c790cfd
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-03-21 13:22:44 -0400

    added resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,8 +27,8 @@ export const AboutSection = () => {
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                             <a href="#contact" className="cosmic-button">Get In Touch</a>
-                            <a href="../assets/MuhammadMoinUddinResume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download>
-                                Download CV
+                            <a href="../../public/MuhammadMoinUddinResume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download>
+                                Download Resume
                             </a>
                         </div>
                     </div>
